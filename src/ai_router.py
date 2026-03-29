ef9a8de0047083cd03b39d33fc910e1738132a8a
jawalekar108-alt <jawalekar108@gmail.com>
2026-03-29 10:29:47 +0530

    Update ai_router.py

--- src/ai_router.py
+++ src/ai_router.py
@@ -3,10 +3,7 @@ from src.gemini_client import gemini_summary
 
 def generate_summary(text):
 
-    # Try GROQ first (fast)
     try:
-
-        print("Trying Groq")
 
         return groq_summary(text)
 
@@ -14,18 +11,12 @@ def generate_summary(text):
 
         print("Groq failed:",e)
 
-    # Fallback Gemini
     try:
-
-        print("Trying Gemini")
 
         return gemini_summary(text)
 
     except Exception as e:
 
         print("Gemini failed:",e)
-
-    # Final fallback
-    print("Using basic summary")
 
     return text[:1500]
