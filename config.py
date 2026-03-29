e124d69b30b5a07a46e22fa5bd011500bb25ac9d
jawalekar108-alt <jawalekar108@gmail.com>
2026-03-29 10:28:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 
+GROQ_API_KEY=os.getenv("GROQ_API_KEY")
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_API_KEY=os.getenv("GEMINI_API_KEY")
