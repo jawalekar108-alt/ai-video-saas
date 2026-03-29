61dfd9551815dfa6e160c047d4dc2ab458e5ae3b
jawalekar108-alt <jawalekar108@gmail.com>
2026-03-29 10:15:46 +0530

    Update transcribe.py

--- src/transcribe.py
+++ src/transcribe.py
@@ -1,96 +1,31 @@
+import os
 from groq import Groq
 from config import GROQ_API_KEY
-from src.split_audio import split_audio
-from src.checkpoint import load_checkpoint,save_checkpoint
-import time
-import os
 
-client=Groq(api_key=GROQ_API_KEY)
+client = Groq(api_key=GROQ_API_KEY)
 
-def transcribe_audio(file_path):
+def transcribe_audio(file):
 
-    chunks=split_audio(file_path)
+    if not os.path.exists(file):
 
-    checkpoint=load_checkpoint()
+        raise Exception("Audio file missing")
 
-    full_text=[]
+    try:
 
-    print("Total chunks:",len(chunks))
+        with open(file,"rb") as audio:
 
-    # initial cooldown (prevents first block)
-    time.sleep(8)
+            res = client.audio.transcriptions.create(
 
-    for i,chunk in enumerate(chunks):
+                file=audio,
 
-        if str(i) in checkpoint:
+                model="whisper-large-v3"
 
-            print("Skipping completed:",i)
+            )
 
-            full_text.append(checkpoint[str(i)])
+        return res.text
 
-            continue
+    except Exception as e:
 
-        retry=0
+        print("Transcription error:",e)
 
-        while retry<5:
-
-            try:
-
-                size=os.path.getsize(chunk)/1024/1024
-
-                print("Chunk",i,"size:",round(size,2),"MB")
-
-                if size>24:
-
-                    print("Chunk too large skip")
-
-                    break
-
-                print("Processing chunk",i+1,"/",len(chunks))
-
-                with open(chunk,"rb") as f:
-
-                    res=client.audio.transcriptions.create(
-
-                        file=f,
-
-                        model="whisper-large-v3"
-
-                    )
-
-                text=res.text
-
-                full_text.append(text)
-
-                checkpoint[str(i)]=text
-
-                save_checkpoint(checkpoint)
-
-                # PRODUCTION RATE LIMITER
-                wait=25
-
-                print("Cooling:",wait,"sec")
-
-                time.sleep(wait)
-
-                break
-
-            except Exception as e:
-
-                retry+=1
-
-                wait=retry*45
-
-                print("Retry",retry)
-
-                print("Waiting:",wait)
-
-                time.sleep(wait)
-
-                if retry==5:
-
-                    print("Failed chunk skipping")
-
-                    full_text.append("")
-
-    return " ".join(full_text)
+        raise Exception("Transcription failed")
