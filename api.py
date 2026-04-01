00c7b083da31e02795927d9478fa9a9a9e19fd6f
jawalekar108-alt <jawalekar108@gmail.com>
2026-04-01 11:26:21 +0530

    Create api.py

---
+++ api.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+
+from worker import process_video
+
+from src.job_store import get_job
+
+
+app=FastAPI()
+
+
+@app.get("/process")
+
+def process(url:str):
+
+    job_id=
+
+    process_video(url)
+
+    return {
+
+    "job_id":job_id
+
+    }
+
+
+@app.get("/status")
+
+def status(job_id:str):
+
+    job=get_job(job_id)
+
+    return job
