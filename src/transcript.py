9f6dd1921ba060d6ff7d6fa53c1fee8d1ea736fb
jawalekar108-alt <jawalekar108@gmail.com>
2026-04-02 14:03:45 +0530

    Create transcript.py

---
+++ src/transcript.py
@@ -0,0 +1,16 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+import re
+
+def extract_video_id(url):
+
+    return re.search(r"v=(.{11})", url).group(1)
+
+def get_transcript(url):
+
+    video_id = extract_video_id(url)
+
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+
+    text = " ".join([t['text'] for t in transcript])
+
+    return text
