a7fbebb05f0fc907fb5847417c3ea8fdea71e745
jawalekar108-alt <jawalekar108@gmail.com>
2026-04-02 14:44:23 +0530

    Update transcript.py

--- src/transcript.py
+++ src/transcript.py
@@ -1,6 +1,6 @@
-from youtube_transcript_api import YouTubeTranscriptApi
-from youtube_transcript_api.formatters import TextFormatter
 import re
+from youtube_transcript_api import YouTubeTranscriptApi
+import yt_dlp
 
 def extract_video_id(url):
 
@@ -12,9 +12,7 @@ def extract_video_id(url):
     return match.group(2)
 
 
-def get_transcript(url):
-
-    video_id = extract_video_id(url)
+def get_youtube_transcript(video_id):
 
     try:
 
@@ -28,4 +26,56 @@ def get_transcript(url):
 
     except:
 
-        return "Transcript unavailable for this video"
+        return None
+
+
+def get_yt_dlp_captions(url):
+
+    try:
+
+        ydl_opts = {
+            'quiet': True,
+            'no_warnings': True,
+            'skip_download': True,
+            'writesubtitles': True,
+            'writeautomaticsub': True,
+            'subtitlesformat': 'vtt',
+            'js_runtimes': ['node']
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+
+            info = ydl.extract_info(url, download=False)
+
+            if 'subtitles' in info:
+
+                for lang in info['subtitles']:
+
+                    subs = info['subtitles'][lang]
+
+                    if subs:
+
+                        return "Captions available but extraction limited"
+
+        return None
+
+    except:
+
+        return None
+
+
+def get_transcript(url):
+
+    video_id = extract_video_id(url)
+
+    text = get_youtube_transcript(video_id)
+
+    if text:
+        return text
+
+    text = get_yt_dlp_captions(url)
+
+    if text:
+        return text
+
+    return "Transcript unavailable"
