875e43dc2cfd693051a2b95f470bc8f50fed0467
jawalekar108-alt <jawalekar108@gmail.com>
2026-04-02 14:12:20 +0530

    Update transcript.py

--- src/transcript.py
+++ src/transcript.py
@@ -1,16 +1,31 @@
 from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import TextFormatter
 import re
 
 def extract_video_id(url):
 
-    return re.search(r"v=(.{11})", url).group(1)
+    match = re.search(r"(v=|youtu.be/)([^&?/]+)", url)
+
+    if not match:
+        raise Exception("Invalid URL")
+
+    return match.group(2)
+
 
 def get_transcript(url):
 
     video_id = extract_video_id(url)
 
-    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    try:
 
-    text = " ".join([t['text'] for t in transcript])
+        api = YouTubeTranscriptApi()
 
-    return text
+        transcript = api.fetch(video_id)
+
+        text = " ".join([t.text for t in transcript])
+
+        return text
+
+    except:
+
+        return "Transcript unavailable for this video"
